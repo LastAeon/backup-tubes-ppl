a67249eb12964c213eb04b2e8e50b4b7d90e8bd0
Syihabuddin Yahya Muhammad <13519149@std.stei.itb.ac.id>
2022-03-01 01:21:00 +0000

    Revert "Merge branch '7-be-call-point-filter' into 'main'"
    This reverts merge request !8

--- samset-salma-app/routes/api.php
+++ samset-salma-app/routes/api.php
@@ -1,15 +1,7 @@
 <?php
 
-use App\Models\AsetTanah;
-use App\Models\AsetBangunan;
 use Illuminate\Http\Request;
-use App\Models\AsetKendaraan;
 use Illuminate\Support\Facades\Route;
-use App\Models\AsetFurniturePeralatan;
-use App\Http\Controllers\AsetTanahController;
-use App\Http\Controllers\AsetBangunanController;
-use App\Http\Controllers\AsetKendaraanController;
-use App\Http\Controllers\AsetFurniturePeralatanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,86 +14,6 @@ use App\Http\Controllers\AsetFurniturePeralatanController;
 |
 */
 
-Route::resource('asetTanah', AsetTanahController::class);
-Route::resource('asetBangunan', AsetBangunanController::class);
-Route::resource('asetFurniturPeralatan', AsetFurniturePeralatanController::class);
-Route::resource('asetKendaraan', AsetKendaraanController::class);
-
-// paging
-Route::get("asetBangunan/page/{page}", function($page){
-    $total = 10;
-    return AsetBangunan::offset(($page-1)*$total)->limit($total)->get();
-});
-
-Route::get("asetTanah/page/{page}", function($page){
-    $total = 10;
-    return AsetTanah::offset(($page-1)*$total)->limit($total)->get();
-});
-
-Route::get("asetFurniturPeralatan/page/{page}", function($page){
-    $total = 10;
-    return AsetFurniturePeralatan::offset(($page-1)*$total)->limit($total)->get();
-});
-
-Route::get("asetKendaraan/page/{page}", function($page){
-    $total = 10;
-    return AsetKendaraan::offset(($page-1)*$total)->limit($total)->get();
-});
-
-// filter
-
-Route::post("asetBangunan/filter", function(Request $request){
-    $var = $request->all();
-    $filter = [];
-    for($i = 1; $i <= count($var); $i++){
-        array_push($filter, explode(',', $var[strval($i)]));
-    }
-    $query = AsetBangunan::query();
-
-    foreach ($filter as &$value) {
-        $query->where($value[0], $value[1], $value[2]);
-    }
-    return $query->get();
-});
-
-Route::post("asetTanah/filter", function(Request $request){
-    $var = $request->all();
-    $filter = [];
-    for($i = 1; $i <= count($var); $i++){
-        array_push($filter, explode(',', $var[strval($i)]));
-    }
-    $query = AsetTanah::query();
-
-    foreach ($filter as &$value) {
-        $query->where($value[0], $value[1], $value[2]);
-    }
-    return $query->get();
-});
-
-Route::post("asetFurniturPeralatan/filter", function(Request $request){
-    $var = $request->all();
-    $filter = [];
-    for($i = 1; $i <= count($var); $i++){
-        array_push($filter, explode(',', $var[strval($i)]));
-    }
-    $query = AsetFurniturePeralatan::query();
-
-    foreach ($filter as &$value) {
-        $query->where($value[0], $value[1], $value[2]);
-    }
-    return $query->get();
-});
-
-Route::post("asetKendaraan/filter", function(Request $request){
-    $var = $request->all();
-    $filter = [];
-    for($i = 1; $i <= count($var); $i++){
-        array_push($filter, explode(',', $var[strval($i)]));
-    }
-    $query = AsetKendaraan::query();
-
-    foreach ($filter as &$value) {
-        $query->where($value[0], $value[1], $value[2]);
-    }
-    return $query->get();
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
