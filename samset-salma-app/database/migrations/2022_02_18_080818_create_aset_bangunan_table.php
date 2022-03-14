55ed404d957de48dbd68cfa5912da2189d1ed13e
LastAeon <syihabuddiny@gmail.com>
2022-03-14 09:59:18 +0700

    chore: restore migration

---
+++ samset-salma-app/database/migrations/2022_02_18_080818_create_aset_bangunan_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAsetBangunanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aset_bangunan', function (Blueprint $table) {
+            $table->increments('Idx');
+            $table->string('Nama_Bangunan')->nullable();
+            $table->string('Alamat')->nullable();
+            $table->integer('Luas_Bangunan')->nullable();
+            $table->integer('Jumlah_Lantai')->nullable();
+            $table->integer('Tahun_Dibangun')->nullable();
+            $table->integer('Tahun_Digunakan')->nullable();
+            $table->integer('Nilai_Perolehan')->nullable();
+            $table->integer('Penambahan_Nilai_Manfaat')->nullable();
+            $table->integer('Umur_Ekonomis')->nullable();
+            $table->integer('Lama_Digunakan')->nullable();
+            $table->float('Tarif')->nullable();
+            $table->integer('Akumulasi')->nullable();
+            $table->integer('Nilai_Buku')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aset_bangunan');
+    }
+}
