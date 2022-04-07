1e734fc416c42002d2a3e081c818568b6610d5ae
LastAeon <syihabuddiny@gmail.com>
2022-04-07 09:18:31 +0700

    feat: create user table, not yet hashing

--- samset-salma-app/database/seeders/DatabaseSeeder.php
+++ samset-salma-app/database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             AsetFurniturePeralatanSeeder::class,
             AsetKendaraanSeeder::class,
             AsetTanahSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
