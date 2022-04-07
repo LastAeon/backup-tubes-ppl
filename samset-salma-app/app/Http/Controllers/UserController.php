c4d1134b1344f9c9c773d61a25b16e2fb783145b
LastAeon <syihabuddiny@gmail.com>
2022-04-07 10:01:23 +0700

    feat: destroy route for every model

--- samset-salma-app/app/Http/Controllers/UserController.php
+++ samset-salma-app/app/Http/Controllers/UserController.php
@@ -86,17 +86,17 @@ class UserController extends Controller
     //     //
     // }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+    // /**
+    //  * Update the specified resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function update(Request $request, $id)
+    // {
+    //     //
+    // }
 
     /**
      * Remove the specified resource from storage.
