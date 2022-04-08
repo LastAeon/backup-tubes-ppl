e73b80a3eb2f7e5ad779d7d6bc5ebf5409aa3a32
LastAeon <syihabuddiny@gmail.com>
2022-04-08 18:45:06 +0700

    feat: update seeder and migration

--- samset-salma-app/database/seeders/SeederHelper.php
+++ samset-salma-app/database/seeders/SeederHelper.php
@@ -2,6 +2,7 @@
 namespace Database\Seeders;
 
 use App\Models\AsetTanah;
+use App\Models\ChangeLog;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class SeederHelper
@@ -12,14 +13,8 @@ class SeederHelper
      * @return void
      */
     public function parseCSV($path, $labels, $model){
-        // $temp = $model->logStatus;
-        // echo $model->logStatus;
-        // echo false;
-        // echo "\ndi seeder\n";
-        // $model->logStatus = false;
-        // if(!$model->logStatus){
-        //     echo "lolos\n";
-        // }
+        // store history last index
+        $last_history = ChangeLog::max('id') ?? 0;
         $csvFile = fopen(base_path($path), "r");
   
         $firstline = true;
@@ -35,14 +30,16 @@ class SeederHelper
                     }
                     $i++;
                 }
-                $model->Global_Id = $model->tableCode . $model->Idx;
-                $model->saveQuietly();
+                // $model->Global_Id = $model->tableCode . $model->Idx;
+                $model->save();
                 $model = $model->new();
             }
             $firstline = false;
         }
-   
+
         fclose($csvFile);
-        // $model->logStatus = $temp;
+        // delete created history
+        var_dump($last_history);
+        ChangeLog::where('id', '>', $last_history)->delete();
     }
 }
